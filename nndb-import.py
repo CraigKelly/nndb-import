84809b6e44d8bafbf1403ea8a1522f51c4f267e0
Craig Kelly <craig.n.kelly@gmail.com>
2015-03-14 00:26:17 -0500

    init skel continues

--- nndb-import.py
+++ nndb-import.py
@@ -3,10 +3,29 @@
 """nndb-import.py
 """
 
+#TODO: handle all these files:
+""" Name      Descrip
+    ========= ==========================================
+X   FOOD_DES  food descriptions
+X   NUT_DATA  nutrient data
+    WEIGHT    weights
+    FOOTNOTE  footnotes
+    --------
+    FD_GROUP  food group descriptions
+    LANGUAL   LanguaL Factors
+    LANGDESC  LanguaL Factors descriptions
+X   NUTR_DEF  Nutrient definitions
+    SRC_CD    source code
+    DERIV_CD  data derivation code description
+    DATA_SRC  Sources of data
+    DATSRCLN  Sources of data Link
+"""
+
 import sys
 import os
 import argparse
 import collections
+import functools
 
 try:
     import pymongo
@@ -14,9 +33,40 @@ except:
     sys.stderr.write("\n\nCould not import PyMongo - it is required\n\n")
     raise
 
-#We go ahead and define field names: one day it might be useful to have these
-#accessible to code
-FOOD_DES_FIELDS = [
+def nndb_recs(filename, field_names=None):
+    """Iterator that yields a dictionary of (field-name,value) pairs. Note that
+    the order of field_names is important!
+    """
+
+    if not field_names:
+        raise ValueError("No fields specified for nndb recs")
+
+    #Filter for a single field in the file
+    def filt(f):
+        if len(f) > 1 and f[0] == '~' and f[-1] == '~':
+            f = f[1:-1]
+        return f
+
+    for line in open(filename, "r"):
+        line = line.strip()
+        if len(line) < 1:
+            continue
+        line = line.decode("utf-8", "ignore")
+        yield dict([
+            (name,filt(fld))
+            for name,fld in zip(field_names, line.split('^'))
+        ])
+
+def recs_to_dict(keyfldname, recs):
+    """Given an iterable of recs and a keyfldname, build and return a
+    dictionary of records"""
+    d = dict()
+    for rec in recs:
+        d[rec[keyfldname]] = rec
+    return d
+
+
+food_des_recs = functools.partial(nndb_recs, field_names = [
     "_id", #aka NDB_No
     "food_group_code", #
     "descrip", #
@@ -31,12 +81,12 @@ FOOD_DES_FIELDS = [
     "protein_factor", #factor for calories from protein
     "fat_factor", #factor for calories from fat
     "carb_facto", #factor for calories from carbohydrates
-]
+])
 
-NUT_DATA_FIELDS = [
+nut_data_recs = functools.partial(nndb_recs, field_names = [
     "ndn_num", #aka NDB_No, the _id to FOOD_DES
     "nutrient_id", #aka Nutr_No
-    "nurtient_val", #Num edible portion in 100g
+    "nutrient_val", #Num edible portion in 100g
     "data_point_count", #Num data points used for analysis (0 means calc, etc)
     "std_error", #Num - std err of mean, can be null (if data_point_count < 3)
     "source_code",
@@ -51,27 +101,16 @@ NUT_DATA_FIELDS = [
     "upper_err_bound", #Num - lower 95% error bound
     "statistical_comments",
     "confidence_code", #Indicates overall assessment of quality for sampling, etc
-]
+])
 
-def nndb_recs(filename, field_names):
-    """Iterator that yields a dictionary of (field-name,value) pairs. Note that
-    the order of field_names is important!
-    """
-
-    #Filter for a single field in the file
-    def filt(f):
-        if len(f) > 1 and f[0] == '~' and f[-1] == '~':
-            f = f[1:-1]
-        return f
-
-    for line in open(filename, "r"):
-        line = line.strip()
-        if len(line) < 1:
-            continue
-        yield dict([
-            (name,filt(fld))
-            for name,fld in zip(field_names, line.split('^'))
-        ])
+nutr_def_recs = functools.partial(nndb_recs, field_names = [
+    "nutrient_id", #aka Nutr_No
+    "units", #mg, g, lb, etc
+    "tagname", #INFOODS tag
+    "descrip",
+    "decimal_places",
+    "sr_sort_order", #num
+])
 
 def num(s, filt=float):
     """Helper for numeric fields - we accept a string, convert it using filt,
@@ -92,7 +131,7 @@ def nums(rec, field_names, filt=float):
 
 def process_directory(mongo, dirname):
     #Shorten our code a little
-    j = os.path.join
+    get_fn = lambda fn: os.path.join(dirname, fn)
 
     #First create all the entries with default values from the main file
     #Note that we use upsert - one of main goals is to be restartable and
@@ -100,7 +139,7 @@ def process_directory(mongo, dirname):
     print "Creating entries..."
     count = 0
     survey_stats = collections.defaultdict(int, [("", 0), ("Y", 0)])
-    for entry in nndb_recs(j(dirname, "FOOD_DES.txt"), FOOD_DES_FIELDS):
+    for entry in food_des_recs(get_fn("FOOD_DES.txt")):
         #Just in case we ever decide to use something else for _id
         entry['nbd_num'] = entry['_id']
         #Handle numeric fields
@@ -113,15 +152,52 @@ def process_directory(mongo, dirname):
             'langual_entries': list(),
             'measures': list(),
         })
+
         #Upsert our entry (already have an _id, so this will be an upsert)
         mongo.save(entry)
+
         count += 1
         survey_stats[entry['survey']] += 1
-    print "...Created %d" % count
+        if count % 3000 == 0:
+            print "  Created %7d" % count
+
+    print "...Total Created %7d" % count
     print "Survey stats:"
     for k,v in survey_stats.iteritems():
         print "  %4s: %12d" % (k,v)
     print ""
+
+    print "Reading nutrient defs..."
+    nutr_defs = recs_to_dict(
+        "nutrient_id",
+        nutr_def_recs(get_fn("NUTR_DEF.txt"))
+    )
+
+    print "Loading nutrition items..."
+    count = 0
+    for entry in nut_data_recs(get_fn("NUT_DATA.txt")):
+        nums(entry, [
+            "nutrient_val",
+            "data_point_count",
+            "std_error",
+            "num_studies",
+            "min_value", "max_value",
+            "degrees_freedom",
+            "lower_err_bound", "upper_err_bound",
+        ])
+
+        #Just add the nutrient def info to the entry
+        xtra = nutr_defs[entry["nutrient_id"]]
+        entry.update(xtra)
+
+        mongo.update(
+            { "_id": entry["ndn_num"] },
+            { "$push": {"nutrients": entry} }
+        )
+        count += 1
+        if count % 50000 == 0:
+            print "  nutrient items: %7d" % count
+    print "...Total nutrient items: %7d" % count
 
 
 #TODO: we're going to want a check that all nutrients in an entry have their
